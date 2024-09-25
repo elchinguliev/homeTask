241049ecd729658390c019c5b80c9e205fdc7863
Quliyev Elchin Afqan <Quliy_av90@itstep.edu.az>
2024-09-25 12:09:36 +0400

    done

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,37 +1,85 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import React from "react";
+import { View,Text,StyleSheet,TouchableOpacity } from "react-native";
+export default function RootLayout(){
+  return(
+    <View style={styles.container}>
+      <View style={styles.card}>
+          <Text style={styles.header}>ITAM-1</Text>
+          <Text style={styles.subHeader}>3 members</Text>
+          <Text style={styles.message}>good work </Text>
+          <View style={styles.tags}>
+            <Text style={styles.tag}>Design</Text>
+            <Text style={styles.tag}>UI/UX</Text>
+            <Text style={styles.tag}>Figma</Text>
+          </View>
+          <TouchableOpacity style={styles.acceptButton}>
+              <Text style={styles.buttonText}>Click</Text>
+          </TouchableOpacity>
+      </View>
+    </View>
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
-  );
+    
+  )
 }
+
+const styles =StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor:"#f5f5f5"
+  },
+  card:{
+    width:"90%",
+    backgroundColor:"#fff",
+    borderRadius:10,
+    padding:20,
+    shadowColor:"#000",
+    shadowOffset:{width:0,height:2},
+    shadowOpacity:0.1,
+    shadowRadius:8,
+    elevation:5,
+    borderWidth:1,
+    borderColor:"#e0e0e0",
+    marginBottom:20,
+    },
+    header:{
+      fontSize:18,
+      fontWeight:"bold",
+      color:"#000"
+    },
+    subHeader:{
+      fontSize:14,
+      color:"#666",
+      marginVertical:5,
+    },
+    message :{
+      fontSize:14,
+      color:"#333",
+      marginVertical:10
+    },
+    tag:{
+      backgroundColor:"#e0e0e0",
+      color:"#000",
+      fontSize:12,
+      padding:5,
+      marginRight:8,
+      borderRadius:5
+    },
+    tags:{
+      flexDirection:"row",
+      marginTop:10
+    },
+    acceptButton:{
+      backgroundColor:"#007bff",
+      paddingVertical:10,
+      paddingHorizontal:15,
+      borderRadius:5,
+      marginTop:15,
+      alignItems:"center"
+    },
+    buttonText :{
+      color:"#fff",
+      fontSize:16
+    }
+})
